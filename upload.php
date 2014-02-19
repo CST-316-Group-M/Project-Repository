f9d942bd0e1640bca44e7747460c3c352980c86a
Lauren Krugen <laurenkrugen@gmail.com>
2014-02-18 22:57:16 -0700

    added some level of encapsulation and prep for unit testing

--- upload.php
+++ upload.php
@@ -1,28 +1,37 @@
 
 <html>
 <body>
-<?php	
-	//$s = 5000;
-	if ($_FILES["file"]["error"] > 0)		// setting file size limit to 5K
-	// UPLOAD_ERR_FORM_SIZE
-		{
-		echo "Error uploading: " . $_FILES["file"]["error"];
-		}
-	elseif ($_FILES["file"]["size"] > 5000)	
-		{
-		echo "File size too large: " . $FILES["file"]["size"];
-		}
-	else
-		{
-		echo "'" . $_FILES["file"]["name"] . "'" . " Upload Successful!";
-		//echo "Temp location of the file: " . $_FILES["file"]["tmp_name"];
-		move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/" . $_FILES["file"]["name"]);
-		}
+<?php
+	/*
+	Class 'Upload' with function(s) that check the file size/type/
+	*/
+	class Upload {
 	
+		public function fileCheck() {
+			$sizeLimit = 5000;
+			if ($_FILES["file"]["error"] > 0)
+				{
+				echo "Error uploading: " . $_FILES["file"]["error"];
+				}
+			elseif ($_FILES["file"]["size"] > $sizeLimit)	
+				{
+				echo "Error uploading: File size too large. " . $_FILES["file"]["size"];
+				}
+			else
+				{
+				echo "'" . $_FILES["file"]["name"] . "'" . " Uploaded Successfully!";
+				//echo "Temp location of the file: " . $_FILES["file"]["tmp_name"];
+				move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/" . $_FILES["file"]["name"]);
+				}
+			}
+	}
 	
+	// class UploadTest extends PHPUnit {}
+	// still working on setting up phpunit 
 	
-
-
+	$userFile = new Upload();
+	$userFile->fileCheck();
+	
 
 /*
 Configuration for the server:
@@ -30,6 +39,11 @@ WAMP version 2.4
 this file will be the configuration for the main file system. Users will have
 access to directories which will act as their repositories. Notes for current code:
 	- need to test file size limits, currently working on test case
+	
+Poses several security issues:
+	- users may be able to access any dir/file
+	- currently no authentication
+	
 */
 
 ?>
