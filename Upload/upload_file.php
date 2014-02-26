2c6dcd8240e79ceae71cf2365aaf95c5b56dda3f
Jacob Reber <jareber@asu.edu>
2014-02-26 11:33:46 -0700

    updated and now uploads using PHP

--- Upload/upload_file.php
+++ Upload/upload_file.php
@@ -1,33 +1,24 @@
 <?php
-define("UPLOAD_DIR", "/srv/www/uploads/");
- 
-if (!empty($_FILES["myFile"])) {
-    $myFile = $_FILES["myFile"];
- 
-    if ($myFile["error"] !== UPLOAD_ERR_OK) {
-        echo "<p>An error occurred.</p>";
-        exit;
-    }
- 
-    // ensure a safe filename
-    $name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
- 
-    // don't overwrite an existing file
-    $i = 0;
-    $parts = pathinfo($name);
-    while (file_exists(UPLOAD_DIR . $name)) {
-        $i++;
-        $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
-    }
- 
-    // preserve file from temporary directory
-    $success = move_uploaded_file($myFile["tmp_name"],
-        UPLOAD_DIR . $name);
-    if (!$success) { 
-        echo "<p>Unable to save file.</p>";
-        exit;
-    }
- 
-    // set proper permissions on the new file
-    chmod(UPLOAD_DIR . $name, 0644);
-}
+if ($_FILES["file"]["error"] > 0)
+  {
+  echo "Error: " . $_FILES["file"]["error"] . "<br>";
+  }
+else
+  {
+  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+  echo "Type: " . $_FILES["file"]["type"] . "<br>";
+  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+   echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "upload/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      }
+	}
+?>
