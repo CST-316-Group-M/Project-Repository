790c3919a35eeb098e24dbc907cc1b1342be38a6
Lauren Krugen <laurenkrugen@gmail.com>
2014-02-27 15:03:18 -0700

    Created user on mysql with only select permissions to authenticate for login, having trouble completing authentication query, atm.

--- regis.php
+++ regis.php
@@ -1,57 +1,57 @@
 <html>
 	<body>
-	<?php
-/*
-		//
+ 	<?php 
+	
 			$name = $_POST['name'];
 			$password = $_POST['password'];	
+			
 			if((!isset($name)) || (!isset($password))) { //if the username or password is not set then it requires login info
-		
+				
+				
 		?>
 			<h1>Log In</h1>
 			<form method="post" action="regis.php">
 			<p>Username: <input type="text" name="name"></p>
 			<p>Password: <input type="Password" name="Password"></p>
+			<p><input type="submit" name="submit" value="Log In"></p>
+			</form>
 		
 		<?php
-			} else { 	*/									 
+			} else {  
+			
+													 
+				
 				//connect to database 
-				$con = mysqli_connect("localhost","lauren","password");		// webauth is sql's username and password to access the table,
+				$con = mysqli_connect("localhost","webauth","webauth");		// webauth is sql's username and password to access the table,
 					if (!$con) {											// only has permission to run a select query to match their username
 						echo "Could not connect. ";
 						exit;
 					}
 					else {
 						echo "Connected Successfully!";
+						exit;
 					}
-		/*				
-				//select the database and the table
-				$sel = mysqli_select_db($con);
+						
+				//select the database 
+				$sel = mysqli_select_db($con, "auth");
 					if (!$sel) {
-						die("Could not select: " . mysql_error());
-				}
-				
-				//table creation - THIS AND THE DATABASE CREATION WILL BE IN AN SQL FILE
-				$auth = mysqli_query("CREATE TABLE auth(fn varchar(20), pw varchar(16), primary key (name))");
-				
-				$ins = mysqli_query("insert into auth('user', 'password')");
-					if (!$ins) {
-						echo "Could not insert";
+						echo "Could not select a database. ";
 						exit;
 					}
 				
-				
-				
 				//query for correct user + password combo
-				$query = mysqli_query("select from auth where fn = '".$name."' and pw = '".$password."'");
-				
+				$query = "select count(*) from auth where firstname = '".$name."' and password = '".$password."'";
 					
-			
-			// and password for the website
-			//- search authentication table for appropriate info
-			//- allow access to account resources
-			//	}
-			*/
+				$result = mysqli_query($con, $query);
+					if (!$result) {
+						echo "Could not run query. ";
+						exit;
+						}
+					else {
+						echo "Query Successful!";
+						exit;
+					}
+			}
 		?>	
 		</body>
 </html>
