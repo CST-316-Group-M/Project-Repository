7780297e30bb95c9b18f061d0b32bd02f803fdc9
Lauren Krugen <LaurenKrugen@gmail.com>
2014-03-31 21:00:40 -0700

    logout is completed, destroys user session

--- logout.php
+++ logout.php
@@ -6,6 +6,13 @@
 			session_destroy(); 
 			//button submission will reference this page, unsets all 
 			//session variables and destroys session
+			echo nl2br("<h2>You have been logged out!</h2>\n<p>See you next time!</p>");
+					
+			if(!isset($_SESSION)) {
+			echo "No session variables set.";
+			}
+			
+			
 		?>
-	<body>
+	</body>
 </html>
