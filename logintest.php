01354d3620914ccf669085664fe1e7562d7d6e9d
Lauren Krugen <laurenkrugen@gmail.com>
2014-04-04 22:18:07 -0700

    methods need implementations

---
+++ logintest.php
@@ -0,0 +1,29 @@
+/*
+Original Author: Lauren Krugen
+Contributing Author(s):
+
+- PHPUnit test file for login.php. 
+*/
+
+<?php
+
+class LoginTest extends PHPUnit_Framework_TestCase {
+	
+	public function sessionTest() {
+		
+	}
+
+	public function postTest() {
+
+	}
+
+	public function connectTest() {
+		
+	}
+
+	public function validateTest() {
+
+	}
+}
+
+?>
