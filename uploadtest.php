4a158b3ec6c03707bce145a656ce6142c6d09103
Lauren Krugen <LaurenKrugen@gmail.com>
2014-04-18 20:41:55 -0700

    need to install vfstream to mock filesystem, building tutorial file from phpunit manual

---
+++ uploadtest.php
@@ -0,0 +1,38 @@
+/*
+Original Author: Lauren Krugen
+Contributing Author(s):
+
+- PHPUnit test file for upload-file.php. 
+*/
+
+<?php
+//will need virtual file system in separate file
+
+class UploadTest extends PHPUnit_Framework_TestCase {
+	
+	//creating mock file and directory
+	$fi;
+	$di;
+	public function __construct() {
+		$this->fi;
+	}
+	
+	public function setDirectory() {
+		
+	}
+	
+}
+
+/*
+class DirTestStuff extends PHPUnit_Framework_TestCase {
+	public function setUp() {
+	}
+	
+	public function createTestDir() {
+	}
+	
+	public function tearDown() {
+	}
+}
+
+*/
