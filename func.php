23ddb19d25044e657f5a6782be01a97ad688594e
Eyaad Allam <eyaad.allam@asu.edu>
2014-04-21 15:18:19 -0700

    dynamic list code is completed

--- func.php
+++ func.php
@@ -5,7 +5,8 @@ include_once 'db.php'; // it is including connection
 function connect() // this function connects to mysql  database
 
 {
- // this step can verify host, users, and password are correct before the connection; otherwise, connection will be failed if one of them is incorrect  
+ // this step can verify host, users, and password are correct before the connection; otherwise, 
+ //connection will be failed if one of them is incorrect  
 mysql_connect(DB_HOST,DB_USER,DB_PASS) or die('cloud not connect to database'. mysql_error());
 
 mysql_select_db(DB_NAME);
@@ -28,16 +29,12 @@ function query()
 
 {
  // It can select all the items of database
-    $myData = mysql_query("SELECT * FROM users ");
-	
+    $myData = mysql_query("SELECT * FROM users ORDER BY Users ASC");
+	//While &myData loads information from the query, fetch that information....
     while($record = mysql_fetch_array($myData))
-
     {
-
+	 //...and push the information into a list that displays the information retrieved in string from
         echo '<li value ="' . $record['Users'] . '">' . $record['Users'] . '</li>';
-
-
-
     }
 
 }
